ba0450f81805b67058feccec0737f89c85ac6983
Kevin Bloch <kev@codingthat.com>
2020-09-08 15:35:12 +0200

    Get rid of even newer deprecation errors.

--- common/services/mongoose.service.js
+++ common/services/mongoose.service.js
@@ -3,12 +3,10 @@ let count = 0;
 
 const options = {
     autoIndex: false, // Don't build indexes
-    reconnectTries: 30, // Retry up to 30 times
-    reconnectInterval: 500, // Reconnect every 500ms
     poolSize: 10, // Maintain up to 10 socket connections
     // If not connected, return errors immediately rather than waiting for reconnect
     bufferMaxEntries: 0,
-    //geting rid off the depreciation errors
+    // all other approaches are now deprecated by MongoDB:
     useNewUrlParser: true,
     useUnifiedTopology: true
     
